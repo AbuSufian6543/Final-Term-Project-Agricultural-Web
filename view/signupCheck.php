8c23a07aae50f9c769db391170e44ea4497bf2ad
emdad118661 <emdad118661@gmail.com>
2021-08-17 07:52:54 +0600

    final commit 10

--- view/signupCheck.php
+++ view/signupCheck.php
@@ -29,8 +29,7 @@ session_start();
 
 		if(!empty($username) && !empty($email) && !empty($password) && !empty($type) && !is_numeric($username))
 		{
-
-			//save to database
+            if(strlen($password) > 5){
 			$user_id = random_num(20);
 			$query = "insert into users (user_id,username,email,password,type) values ('$user_id','$username','$email','$password','$type')";
 
@@ -38,6 +37,9 @@ session_start();
 
 			header("Location: login.php");
 			die;
+		}else{
+			echo "<script>alert('Password should be more than 4 charecter')</script>";
+		}
 		}else
 		{
 			echo "Please enter some valid information!";
